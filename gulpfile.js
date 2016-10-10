b68a38044f23beed33c9e0561aca700589199ee5
Nazar <Nazar>
2016-10-10 12:40:08 +0300

    Add index.html file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var path = require('path');
 
 gulp.task('less', function () {
   return gulp.src('app/less/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('dist/css'));
+    .pipe( less() )
+    .pipe( gulp.dest('dist/css') );
 });
 
 gulp.task('default', function() {
